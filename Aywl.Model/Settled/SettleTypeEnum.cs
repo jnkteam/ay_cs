aec95a87c9a91dc2a2cb41fdfe31bdc69a8aa297
gdw9888 <42003329@qq.com>
2017-11-21 13:53:46 +0800

    上传新cs

---
+++ Aywl.Model/Settled/SettleTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace OriginalStudio.Model.Settled
+{
+    using System;
+
+    /// <summary>
+    /// 账户操作方式。加款或扣款
+    /// </summary>
+    public enum SettleTypeEnum
+    {
+        加款 = 1,
+        扣款 = 2
+    }
+}
+

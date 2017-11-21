234d5511faca3bc014faa2c2085d325d9e00585c
gdw9888 <42003329@qq.com>
2017-11-21 13:53:22 +0800

    上传新cs

---
+++ Aywl.Model/PayRate/RateTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace OriginalStudio.Model.PayRate
+{
+    using System;
+
+    public enum RateTypeEnum
+    {
+        代理 = 4,
+        会员 = 3,
+        平台 = 1,
+        Proxy = 2
+    }
+}
+
